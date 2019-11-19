c3aab0f0e050581486de7f679df1338168ebf5d1
fedeghe <fedeghe@gmail.com>
2019-11-20 00:32:01 +0100

    head & get & del by id or given key

--- plugins/malta-restify/server.js
+++ plugins/malta-restify/server.js
@@ -10,139 +10,138 @@ const fs = require('fs'),
         preflightMaxAge: 5,
         origins: ['*']
     });
+
 let server = null;
 
 const requireUncached = module => {
-    const mod = require.resolve(path.resolve(module))
-    if (mod && mod in require.cache) {
-        delete require.cache[mod]
-    }
-    const ret = require(path.resolve(module))
-    return ret
-}
-
-
-const put_post = (fname, payload) => {
-    const data = requireUncached(fname)
-    data.push(payload)
-    console.log(data)
-    return fs.writeFileSync(fname, JSON.stringify(data))
-}
-
-const mutate = {
-    del: (fname, id) => {
-        try{
-            const data = requireUncached(fname)
-            console.log("data", data, id)
-            const newData = data.filter(d => {
-                console.log(d)
-                return d.id != id
-            })
-            console.log("newData", newData)
-            fs.writeFileSync(fname, JSON.stringify(newData))
-            return true;
-        } catch(e) {
-            console.log(e)
-            return false;
+        const mod = require.resolve(path.resolve(module))
+        if (mod && mod in require.cache) {
+            delete require.cache[mod]
         }
+        const ret = require(path.resolve(module))
+        return ret || []
     },
-    put:put_post,
-    post:put_post
-}
-
-const getResponder = (verb, filePath, fname) => (req, res , next) => {
-    console.log("verb", verb)
-    console.log("filePath", filePath)
-    res.setHeader('Access-Control-Allow-Origin','*');
-    switch(verb) {
-        case 'del': 
-            if ('id' in req.params) {
-                mutate.del(filePath, req.params.id) && res.send(204);
-            }
-            break;
-        case 'get': 
-            try {
-                res.send(200, requireUncached(fname));
-            }catch(e) {
-                console.log(e)
-            }
-            break;
-        case 'head':
-            res.send(200);
-            break;
-        case 'post': 
-        case 'put':
-            console.log(req)
-            if (!req.is('application/json')) {
-                return next(
-                    new errors.InvalidContentError("Expects 'application/json'"),
-                );
-            }
-            try {
-                console.log(req.params)
-                mutate[verb](filePath, req.body)
-                && res.send(204);
+    put_post = (fname, payload) => {
+        let data = requireUncached(fname)
+        if (payload instanceof Array) {
+            data = data.concat(payload)
+        } else {
+            data.push(payload)
+        }
+        return fs.writeFileSync(fname, JSON.stringify(data))
+    },
+    mutate = {
+        del: (fname, id, k) => {
+            try{
+                const data = requireUncached(fname),
+                    newData = data.filter(d => d[k] != id)
+                fs.writeFileSync(fname, JSON.stringify(newData))
+                return true;
             } catch(e) {
                 console.log(e)
+                return false;
             }
-            res.send(404);
-            break;
-        default: break;
-    }
-    return next();
-}
-
-const start = (port, host, folder, endpoints) => {
-    try{
-        fs.readFile(path.resolve(folder, endpoints), 'utf8', (err, data) => {
-            const eps = JSON.parse(data)
-            if (err) throw 'Error reading endpoint file'
-            Object.keys(eps).forEach(verb => {
-
-                /**
-                 * 
-                 * Verbs here are 
-                 * del, get, head, opts, post, put, and patch
-                 * 
-                 */
-
-
-                eps[verb].forEach(ep => {
-                    const fpath = path.resolve(folder, ep.file)
+        },
+        put:put_post,
+        post:put_post
+    },
+    head = (fname, res) => {
+        const content = requireUncached(fname);
+        res.setHeader('content-length', content.toString().length);
+        res.setHeader('content-type', 'application/json');
+    },
+    getResponder = (verb, filePath, ep) => (req, res , next) => {
+        const fname = ep.file;
+        res.setHeader('Access-Control-Allow-Origin','*');
+        res.setHeader('Server','malta-restify');
+        const k = ep.key || 'id'
+        switch(verb) {
+            case 'del': 
+                if (k in req.params) {
+                    mutate.del(filePath, req.params.id, k) && res.send(204);
+                }
+                break;
+            case 'get': 
+                try {
                     
-                    try {
-                        server[verb]({path : ep.ep} , getResponder(verb, fpath, ep.file));
-                    } catch(e) {
-                        console.log('Error' ,e)
+                    if (k in req.params) {
+                        let r = requireUncached(fname)
+                        res.send(200, r.filter(e => e[k] == req.params[k]));
+                    } else {
+                        res.send(200, requireUncached(fname));
                     }
-                })
-            })
-            server.listen(port, host, () => {
-                console.log('%s listening at %s ', server.name, server.url);
-            });
-            console.log('start server')
-        });
-    }catch(e) {
-        console.log(e)
-    }
-    
-}
-
-const getServer = () => {
-    console.log('getting server')
-    server = restify.createServer();
-    server.use(plugins.queryParser());
-    server.use(restifyBodyParser());
-    server.pre(cors.preflight);
-    server.use(cors.actual);
-    return {
-        start: (server !== null)
-        ? start
-        : () => {
-            throw 'Server not created'
+                }catch(e) {
+                    console.log(e)
+                }
+                break;
+            case 'head':
+                head(fname, res);
+                res.send(200);
+                break;
+            case 'post': 
+            case 'put':
+                if (!req.is('application/json')) {
+                    return next(
+                        new errors.InvalidContentError("Expects 'application/json'"),
+                    );
+                }
+                try {
+                    mutate[verb](filePath, req.body)
+                    && res.send(204);
+                } catch(e) {
+                    console.log(e)
+                }
+                res.send(404);
+                break;
+            default: break;
         }
-    }
-}
+        return next();
+    },
+    start = (port, host, folder, endpoints) => {
+        try{
+            fs.readFile(path.resolve(folder, endpoints), 'utf8', (err, data) => {
+                const eps = JSON.parse(data)
+                if (err) throw 'Error reading endpoint file'
+                Object.keys(eps).forEach(verb => {
+                    /**
+                     * 
+                     * Verbs here are 
+                     * del, get, head, opts, post, put, and patch
+                     * 
+                     */
+                    eps[verb].forEach(ep => {
+                        const fpath = path.resolve(folder, ep.file)
+                        try {
+                            server[verb]({path : ep.key ? ep.ep.replace(/\:id/, `:${ep.key}`) : ep.ep} , getResponder(verb, fpath, ep));
+                        } catch(e) {
+                            console.log('Error' ,e)
+                        }
+                    })
+                })
+                server.listen(port, host, () => {
+                    console.log('%s listening at %s ', server.name, server.url);
+                });
+                console.log('start server')
+            });
+        }catch(e) {
+            console.log(e)
+        }    
+    },
+    getServer = () => {
+        console.log('- getting server')
+        server = restify.createServer();
+        server.use(plugins.queryParser());
+        server.use(restifyBodyParser());
+        server.pre(cors.preflight);
+        server.use(cors.actual);
+        return {
+            start: (server !== null)
+            ? start
+            : () => {
+                throw ':[] server not created'
+            }
+        }
+    };
 
 module.exports = {
     getServer
