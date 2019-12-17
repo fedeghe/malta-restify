bd6cf098cd9c2608b9dfaff631e3d9613a6e68a6
fedeghe <fedeghe@gmail.com>
2019-12-17 07:38:45 +0100

    small fix

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var srv = require('./server'),
     path = require('path'),
     fs = require('fs');
 
-server = srv.getServer();
-// console.log(server)
 function malta_restify(obj, options = {}) {
-    const self = this,
+    const server = srv.getServer(),
+        self = this,
         start = new Date(),
-        endpoints = options.endpoints,
         folder = path.resolve(process.cwd(), options.folder || './'),
         host = options.host || '127.0.0.1',
-        port = options.port || 3001;
+        port = options.port || 3001,
+        endpoints = options.endpoints;
+        
     let msg;
+
     server.start({port, host, folder, endpoints, malta: self});
-    // fs.existsSync(endpoints) && self.listen(endpoints)
     return function (solve, reject) {
         solve(obj);
         self.notifyAndUnlock(start, msg);
